e8551d5c5f715f86b59771802a6a2d8a4925d1fb
Jomarie Baris <165636052+BarisJomarie@users.noreply.github.com>
2025-12-04 05:29:18 +0800

    Set CORS for Netlify frontend access
    Configured CORS to allow requests from Netlify frontend.

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,11 @@ const publicationRoutes = require("./routes/publicationRoutes");
 
 
 
-app.use(cors());
+// Allow your Netlify frontend
+app.use(cors({
+  origin: "https://sdgclassification.netlify.app", // your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
